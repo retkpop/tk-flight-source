e39320f82a84eadeaea7174e08bd2018544eda1c
TamKieu <kvttam@gmail.com>
2019-11-26 23:08:05 +0700

    Upload Source

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -6,9 +6,19 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@app/material.module';
 
 import { ShellComponent } from './shell.component';
+import { MatIconModule } from '@angular/material';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, FlexLayoutModule, MaterialModule, RouterModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    FlexLayoutModule,
+    MaterialModule,
+    RouterModule,
+    MatIconModule,
+    AngularFontAwesomeModule
+  ],
   declarations: [ShellComponent]
 })
 export class ShellModule {}
